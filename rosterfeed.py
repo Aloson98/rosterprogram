41c394ffca739cd1be7f6af982c0134b03dca4f8
grayson aloyce <55749230+Aloson98@users.noreply.github.com>
2025-02-19 18:52:21 +0300

    Update rosterfeed.py

--- rosterfeed.py
+++ rosterfeed.py
@@ -2,16 +2,19 @@
 Here are the data to be feed to the roster for testing
 """
 
+#provide list of the staff names you have
 staff = [
     "grayson", "weston", "patrick", "mwaisaka", "lugano", "mkumba", "martin", "beldina", "felister", "nyandaro", "dorice", "magreth",
     "joshua", "jordan", "geofrey", "hansfrida", "Mariam"
     ]
 
+#For the starting week, provide the must have monday duties based on the ending roster
 special_names = {
     "grayson": "SD",
     "lugano": "DO",
     "mkumba": "SD"
 }
 
+#automatically calculate the number of morning shift and night shift required on your roster
 req_m = (len(staff) * 4) // 15
 req_en = (len(staff) * 1) // 5
